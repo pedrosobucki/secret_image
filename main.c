57807a72fe3f2fc48ceae3d7ca9e53b82c7163b2
pedrosobucki <pedrosobucki@gmail.com>
2023-09-23 16:39:04 -0300

    implemented decoding of secret image dimentions

--- main.c
+++ main.c
@@ -6,21 +6,23 @@ void hide_pixel(char *bs, Pixel tohide);
 
 void retrieve_size(Pixel *pixel, unsigned int *height, unsigned int *width)
 {
-    unsigned int h, w;
+    unsigned int h = 0, w = 0;
 
     char mask, sinfo; 
     char *px = pixel;
 
     for (unsigned int i=0; i < 6; i++) {
-        sinfo = *px++ & 0x3;  // retrieves 2 least meaningful bits from color
-        h |= sinfo;
         h <<= 2;
+        sinfo = *px & 0x3;  // retrieves 2 least meaningful bits from color
+        h |= sinfo;
+        px++;
     }
 
     for (unsigned int i=0; i < 6; i++) {
-        sinfo = *px++ & 0x3;  // retrieves 2 least meaningful bits from color
-        w |= sinfo;
         w <<= 2;
+        sinfo = *px & 0x3;  // retrieves 2 least meaningful bits from color
+        w |= sinfo;
+        px++;
     }
 
     *height = h;
@@ -35,8 +37,7 @@ int main()
     char *secret_name = "british_summer.ppm";
 
     Img data1, data2;
-    Img *base = &data1;
-    Img *secret = &data2;
+    Img *base = &data1, *secret = &data2;
 
     int r = read_ppm(base_name, base) + read_ppm(secret_name, secret);
 
@@ -66,7 +67,8 @@ int main()
     unsigned char *bp = base->pix + 12;
     unsigned char s;
 
-    printf("width: %d, height: %d\n", base->width, base->height);
+    printf("BASE width: %d, height: %d\n", base->width, base->height);
+    printf("SECRET width: %d, height: %d\n", secret->width, secret->height);
     for (unsigned int i = 0; i < secret_size; i++)
     {
         hide_pixel(bp, secret->pix[i]);
@@ -79,17 +81,17 @@ int main()
     free_ppm(base);
     free_ppm(secret);
 
-    // r = read_ppm(encoded_name, base);
+    r = read_ppm(encoded_name, base);
 
-    // if (r != 0) {
-    //     printf("Failed to read '%s'.\n", encoded_name);
-    //     return -1;
-    // }
+    if (r != 0) {
+        printf("Failed to read '%s'.\n", encoded_name);
+        return -1;
+    }
 
-    // unsigned int height, width;
-    // retrieve_size(base->pix, height, width);
+    unsigned int height, width;
+    retrieve_size(base->pix, &height, &width);
 
-    // printf("retrieve size: h %u X w %u\n", height, width);
+    printf("retrieve size: h %u X w %u\n", height, width);
 }
 
 void hide_pixel(char *bs, Pixel tohide)
@@ -119,10 +121,10 @@ void hide_size(Pixel *pixel, unsigned int height, unsigned int width)
     for (unsigned int i=0; i < 12; i++) {   // since we store 2 secret bits in every byte, we iterate 12 times for 24 bits stored
         rgb = *(px+i);  // stores current pixel ptr value (points to specific color value)
         rgb &= 0xFC;    // clears last 2 bits of color value
-        mask = size & 0x3;  // creates mask with only 2 least significant bits from size
+        mask = (size >> 22 - 2*i) & 0x3;  // creates mask with only 2 least significant bits from size
         // printf("%p(%u) -> %02x + %02x = %02x\n", px+i, i, *(px+i), mask, rgb | mask);
         *(px+i) = rgb | mask;   // stores OR between rgb and 2 size bits in color value
-        size >>= 2; // shifts size value 2 bits to the right, now storing the next 2 bits to be hiden in the least significant bits
+        // size >>= 2; // shifts size value 2 bits to the right, now storing the next 2 bits to be hiden in the least significant bits
     }
     // printf("loop end---------------\n");
 }
